f4bcb433a432804c2485756e88b8a16f283b1f33
carlosbenavides123 <carlos7legend@gmail.com>
2018-07-13 11:25:29 -0700

    INITIAL COMMIT

---
+++ app/Http/Controllers/ClassesController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlos Benavides
+ * Date: 7/13/2018
+ * Time: 11:24 AM
+ */
+
+namespace App\Http\Controllers;
+
+
+class ClassesController extends Controller
+{
+
+}
