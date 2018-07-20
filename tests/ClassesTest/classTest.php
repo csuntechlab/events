1e7a10d791c1e951542f40d88200ee1a196a492e
carlosbenavides123 <carlos7legend@gmail.com>
2018-07-19 17:55:48 -0700

    Tests update to work with class, final retriever

--- tests/ClassesTest/classTest.php
+++ tests/ClassesTest/classTest.php
@@ -21,9 +21,35 @@ class classTest extends TestCase
         $this->retriever = Mockery::spy(ClassContract::class);
     }
 
-    public function get_json_from_local_db()
-    {
 
+    /**
+     * @test
+     */
+    public function test_the_final_method()
+    {
+        $data = [
+            'pattern_number'=> 1,
+            'type' => "final",
+            "label" => "Final Meeting Time",
+            "description" => null,
+            "start_time" => "1000h",
+            "end_time" => "1225h",
+            "days" => "TH",
+            "from_date" => "2018-12-15",
+            "to_date" => "2018-12-15",
+            "location_type"=> "physical",
+            "location "=> "Not AC210",
+            "booking_url" => "Prob wont need this...",
+            'online_label' => "And this...",
+            "online_url" => "or this...",
+        ];
+
+        $this->retriever
+             ->shouldReceive('finalInfo')
+             ->with(2187,16258)
+             ->andReturn(json_encode($data));
+
+        $this->assertEquals(json_encode($data),$this->retriever->finalInfo(2187,16258));
     }
 
     /**
@@ -61,8 +87,7 @@ class classTest extends TestCase
      */
     public function test_classes_route_GET_expect_200()
     {
-        $response = $this->call('GET', '1.0/terms/2323/classes/4324234'); // (goes to '/classes/3')
+        $response = $this->call('GET', '1.0/terms/2323/classes/12345'); // searches for term 2323, and course id 12345
         $this->assertEquals(200, $response->status());
     }
-
 }
--- tests/Controllers/ClassControllerTest.php
+++ tests/Controllers/ClassControllerTest.php
@@ -23,35 +23,76 @@ class ClassControllerTest extends \TestCase
     /** @test */
     public function getCourseDetails_with_course_id()
     {
-        $this->retriever
-            ->shouldReceive('course_details')
-            ->with(2187,16258)
-            ->andReturn(json_encode(['class_details' => [
-                'Locate' => 'JD3504',
-                'events' => [
-                    'Class Start' =>'Aug 25,2018',
-                    'Class End' => 'Dec 11,2018',
-                    'Attendance' => [
-                        'Friday' => '9:00 am to 12:15 pm'
-                    ],
-                    'Final Exam' => 'Dec 15,2018'
-                ]
-            ]]));
-
-        $controller = new ClassController($this->retriever);
-        $returnArray = [
-            'Locate' => 'JD3504',
-            'events' => [
-                'Class Start' =>'Aug 25,2018',
-                'Class End' => 'Dec 11,2018',
-                'Attendance' => [
-                    'Friday' => '9:00 am to 12:15 pm'
-                ],
-                'Final Exam' => 'Dec 15,2018'
+        $data = [
+        'pattern_number'=> 1,
+        'type' => "class",
+        "label" => "Class Meeting Time",
+        "description" => null,
+        "start_time" => "0800h",
+        "end_time" => "0825h",
+        "days" => "MW",
+        "from_date" => null,
+        "to_date" => null,
+        "location_type"=> "physical",
+        "location "=> "AC210",
+        "booking_url" => null,
+        'online_label' => null,
+        "online_url" => null,
+        "get_details" => [
+        "term_id" => 2153,
+            "term"=> "Spring-2015",
+            "course_id"=> 19009,
+            "subject"=> "ART",
+            "catalog_number"=> "100",
+            "title"=> "Introduction to Art Processes",
+            "section_number"=> "01"
             ]
         ];
-        $output = $controller->courses(2187,16258);
-        $this->assertEquals($output, json_encode(['class_details' =>$returnArray]));
+
+
+        $this->retriever
+            ->shouldReceive('classInfo')
+            ->with(2187,16258)
+            ->andReturn(json_encode($data));
+
+        $controller = new ClassController($this->retriever);
+        $output = $controller->classInfo(2187,16258);
+
+        $this->assertEquals(json_encode($data), $output);
+    }
+
+
+    /**
+     * @test
+     */
+    public function controller_test_the_final_info_retriever()
+    {
+        $data = [
+            'pattern_number'=> 1,
+            'type' => "final",
+            "label" => "Final Meeting Time",
+            "description" => null,
+            "start_time" => "1000h",
+            "end_time" => "1225h",
+            "days" => "TH",
+            "from_date" => "2018-12-15",
+            "to_date" => "2018-12-15",
+            "location_type"=> "physical",
+            "location "=> "Not AC210",
+            "booking_url" => "Prob wont need this...",
+            'online_label' => "And this...",
+            "online_url" => "or this...",
+        ];
+
+        $this->retriever
+            ->shouldReceive('finalInfo')
+            ->with(2187,16258)
+            ->andReturn(json_encode($data));
+
+        $controller = new ClassController($this->retriever);
+        $result = $controller->finalInfo(2187,16258);
+
+        $this->assertEquals(json_encode($data),$result);
     }
 
 }
